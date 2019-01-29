5e6d4761f1eb3fbeb2ac6568ed37fc6aa8000c90
sheridanb2008 <31253905+sheridanb2008@users.noreply.github.com>
2019-01-28 20:58:53 -0800

    f

--- CST 363_project1/cgi-bin/Recall.py
+++ CST 363_project1/cgi-bin/Recall.py
@@ -20,8 +20,8 @@ if "submit" in form:
 print("Content-Type: text/html")    # HTML is following
 print()                             # blank line required, end of headers
 print('<TITLE>Recall Notices</TITLE>')
-
-product_id_sql = 'select product_id from recall_notices where product_id = %s'
+get_recall_sql = 'select notice_id from recall_notices where product_id = %s and affected_start_date = %s and affected_end_date = %s and issue_description = %s'
+product_id_sql = 'select product_id from product_type where product_id = %s'
 insert_sql = 'insert into recall_notices (notice_id, product_id, affected_start_date, affected_end_date, issue_description) values (DEFAULT, %s, %s, %s, %s)'
 
 # connect to database
@@ -30,17 +30,21 @@ try:
                                 password='sesame',
                                 database='mrs_project1',
                                 host='127.0.0.1')
-
     cursor = cnx.cursor()
-    cursor.execute(product_id_sql,[product_id])
-    row = cursor.fetchone()    
-    if row is None and submit is True:
-        cursorb = cnx.cursor()
-        cursorb.execute(insert_sql, (product_id, affected_start_date, affected_end_date, issue_description))
-        cnx.commit()
-        print('Thank you for submitting the recall')
+    cursor.execute(get_recall_sql,(product_id,affected_start_date,affected_end_date,issue_description))
+    row = cursor.fetchone()
+    if row is None:
+        cursora = cnx.cursor()
+        cursora.execute(product_id_sql,[product_id])
+        row = cursora.fetchone()    
+        if row[0] == int(product_id) and submit is True:
+            cursorb = cnx.cursor()
+            cursorb.execute(insert_sql, (product_id, affected_start_date, affected_end_date, issue_description))
+            cnx.commit()
+            print('Thank you for submitting the recall')
     elif row is not None and submit is True:
-        print('That Recall Already Exists!')
+            print('That Recall Already Exists!')
+       
 except mysql.connector.Error as err:
     print ("ERROR", err)
 cnx.close()  # close the connection 
